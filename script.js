446b37f998d9062bfcd43501749c553216f3c5d7
Abhishek Pal <paljiabhishek184@gmail.com>
2024-02-26 13:16:39 +0530

    upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let box = document.getElementById("box");
+
+box.style.width = "300px";
+box.style.height = "200px";
+box.style.background ="beige";
+box.style.margin = "20px";
+box.style.padding = "10px";
+box.style.fontSize = "18px";
+box.style.fontWeight ="bold";
